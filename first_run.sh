7a7ce0fbe68cb1033f210ec16b03e0855ad6df91
Hamada, Ondrej <ondrej@hamada.cz>
2016-03-25 23:45:33 +0100

    Check Postgres availability

--- first_run.sh
+++ first_run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+MAX_CHECK=6
+SLEEP=5
+
 # system is already configured, terminate
 if grep -q '@DB_USER@' /etc/grafana/grafana.ini; then
 
     [ -z "$PGPASSWORD" ] && exit 1
     [ -z "$PGHOST" ] && exit 1
-    [ -z "PGUSER" ] && exit 1
+    [ -z "$PGUSER" ] && exit 1
 
     DB_USER="${DB_USER:-graphite}"
     DB_PASSWORD="${DB_PASSWORD:-graphite}"
@@ -22,6 +25,21 @@ if grep -q '@DB_USER@' /etc/grafana/grafana.ini; then
                -e "s/@DB_GRAFANA_NAME@/$DB_GRAFANA_NAME/" \
                -e "s/@DB_GRAPHITE_NAME@/$DB_GRAPHITE_NAME/" $CONFIG_FILE
     done
+
+    CHECK_CNT=0
+    while [ "$CHECK_CNT" -leq "$MAX_CHECK" ]; do
+        psql -lqt 1>/dev/null 2>&1
+        if [ "$?" -eq 0 ]; then
+            break
+        fi
+        CHECK_CNT=$(($CHECK_CNT + 1))
+        sleep $SLEEP
+    done
+
+    if [ "$CHECK_CNT" -geq "$MAX_CHECK" ]; then
+        echo "Could not contact database in $(($SLEEP * $MAX_CHECK)) seconds."
+        exit 1
+    fi
 
     if ! psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER'" | grep -q 1; then
         psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
